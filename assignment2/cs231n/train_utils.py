68a6f7a5c773b82c2bd4762fd391712a260c4f08
Jake Lussier <jlussier@Jakes-MacBook-Pro-2.local>
2018-09-15 11:54:41 -0700

    added hyperopt functionality. need to actually run and debug.

--- assignment2/cs231n/train_utils.py
+++ assignment2/cs231n/train_utils.py
@@ -50,92 +50,6 @@ def train(model, optimizer, loader_train, loader_val, epochs=1, its=None, eval_i
             if break_its: break
     return history
 
-class History(object):
-    def __init__(self):
-        self.losses = []
-        self.train_accs = []
-        self.val_accs = []
-        self.preds = []
-    def update(self, loss, train_acc, val_acc, preds):
-        self.losses.append(loss)
-        self.train_accs.append(train_acc)
-        self.val_accs.append(val_acc)
-        if self.preds:
-            assert(preds.shape == self.preds[-1].shape)
-        self.preds.append(preds)
-    @property
-    def train_acc(self):
-        return self.train_accs[-1] if self.train_accs else None
-    @property
-    def val_acc(self):
-        return self.val_accs[-1] if self.val_accs else None
-    @property
-    def bad(self):
-        return (self.val_accs[-1] - self.val_acs[0]) < 5.
-
-class ChoiceSampler(object):
-    def __init__self(self, choices):
-        self.bad = np.zeros(len(choices))
-        self.tot = np.zeros(len(choices))
-        self.choices = choices
-    def sample(self):
-        good = (self.bad / (self.tot+1)) < 0.75
-        assert(np.sum(good))
-        ind = np.choice(np.nonzero(good)[0], 1)
-        return self.choices[ind]
-    def update(self, bad, val):
-        ind = [i for (i,v) in enumerate(self.choices) if abs(v-val) < 1e-9][0]
-        self.bad[ind] += bad
-        self.tot[ind] += 1
-
-class SetSampler(object):
-    """
-    Samples sets of parameters according to a schedule and taking into account
-    past performance.
-
-    Example usage:
-    
-    samplers = {
-      "FilterSize": ChoiceSampler([3, 5]),
-      "FilterCount": ChoiceSampler([8, 32, 128]),
-    }
-    sampler = SetSampler(samplers, fine_epochs=5)
-    while sampler.num_complete < 10: # until 10 sets have each run for 5 epochs
-      parameters = sampler.sample_parameters()
-      model = construct_model(parameters) # assume you write
-      history = train(model, loader_train, loader_val) # assume you write
-      sampler.update(parameters, history)    
-    """
-    def __init__(self, samplers, num_active=None, course_its=None, fine_epochs=10,
-                 course_fine="contract"):
-        """
-        Construct a SetSampler object.
-        
-        Required arguments:
-        - samplers: a dictionary from 
-        Convenience layer that perorms an affine transform followed by a ReLU
-        
-        Inputs:
-        - x: Input to the affine layer
-        - w, b: Weights for the affine layer
-        
-        Returns a tuple of:
-        - out: Output from the ReLU
-        - cache: Object to give to the backward pass
-        """
-        self.samplers = samplers
-        self.results = []
-        self.active = []
-        self.num_active = 
-        self.course_its = course_its
-        self.fine_epochs = fine_epochs
-    def sample_parameters(self):
-        while 
-        return dict([(n, p.sample()) for (n, s) in self.samplers.items()])
-    def update(parameters, hist):
-        [self.samplers[n].update(hist.bad, v) for (n, v) in parameters.items()]
-
-
 def flatten(x):
     N = x.shape[0] # read in N, C, H, W
     return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
@@ -143,7 +57,7 @@ def flatten(x):
 class Flatten(nn.Module):
     def forward(self, x):
         return flatten(x)
-            
+
 def construct_model(parameters, im_shape):
 
     (channels, im_size, im_size2) = im_shape
@@ -178,3 +92,133 @@ def construct_model(parameters, im_shape):
         in_dim = out_dim
 
     return nn.Sequential(*layers)
+
+class History(object):
+    def __init__(self):
+        self.losses = []
+        self.train_accs = []
+        self.val_accs = []
+        self.preds = []
+    def update(self, loss, train_acc, val_acc, preds):
+        self.losses.append(loss)
+        self.train_accs.append(train_acc)
+        self.val_accs.append(val_acc)
+        if self.preds:
+            assert(preds.shape == self.preds[-1].shape)
+        self.preds.append(preds)
+    @property
+    def train_acc(self):
+        return self.train_accs[-1] if self.train_accs else None
+    @property
+    def val_acc(self):
+        return self.val_accs[-1] if self.val_accs else None
+    @property
+    def bad(self):
+        return (self.val_accs[-1] - self.val_acs[0]) < 5.
+    @property
+    def working(self):
+        return (self.val_accs[-1] - self.val_acs[0]) > 5.
+
+class ChoiceSampler(object):
+    def __init__self(self, choices):
+        self.bad = np.zeros(len(choices))
+        self.tot = np.zeros(len(choices))
+        self.choices = choices
+    def sample(self):
+        good = (self.bad / (self.tot+1)) < -1#0.75
+        assert(np.sum(good))
+        ind = np.choice(np.nonzero(good)[0], 1)
+        return self.choices[ind]
+    def update(self, bad, val):
+        ind = [i for (i,v) in enumerate(self.choices) if abs(v-val) < 1e-9][0]
+        self.bad[ind] += bad
+        self.tot[ind] += 1
+
+class HyperOpt(object):
+    """
+    A hyperparameter optimizer. Includes individual parameter samplers,
+    training routines, and coarse/fine strategies.
+    
+    Example usage:
+
+    samplers = {
+      "FilterSize": ChoiceSampler([3, 5]),
+      "FilterCount": ChoiceSampler([8, 32, 128]),
+    }
+    ho = HyperOpt(samplers)
+    while True:
+      ho.step()
+      print("Best parameters:", ho.best_parameters)
+    """
+    def __init__(self, samplers, constructor, trainer, loader_train, loader_val,
+                 max_active=None, coarse_its=None, fine_epochs=10,
+                 coarse_fine="contract"):
+        """
+        Construct a HyperOpt object.
+        
+        Required arguments:
+        - samplers: Dictionary from parameter name to parameter Sampler
+        - constructor: Model constructor function. Must have the following API:
+          - constructor(parameters, image_shape)
+        - trainer: Model training function. Must have the following API:
+          - trainer(model, loader_train, loader_val)
+        - trainer_val: PyTorch DataLoader for training set.
+        - loader_val: PyTorch DataLoader for validation set.
+        - max_active: Maximum number of active parameter sets.
+        - coarse_its: Number of training iterations during coarse phases.
+        - fine_epochs: Number of training epochs during fine phases.
+        - coarse_fine: Coarse/fine strategy. One of the following:
+          - contracting: Generate max_active and then complete or kill all,
+            before repeating.
+          - filling: Always keep active at max_active.
+        """
+        self.samplers = samplers
+        self.constructor = constructor
+        self.trainer = trainer
+        self.max_active = max_active
+        self.coarse_its = coarse_its
+        self.fine_epochs = fine_epochs
+        self.coarse_fine = coarse_finne
+        self.fine = False # begin coarse
+        self.active = queue.PriorityQueue() # begin with no active
+        self.results = [] # begin with no results
+    def step(self):
+        parameters = self.sample()
+        model = self.constructor(parameters, 32) # TODO: don't hard code?
+        if coarse_fine == "contract":
+            # See if we need to reset fine.
+            if not self.fine and len(self.active) == self.max_active:
+                self.fine = True
+            elif self.fine and not self.active:
+                self.fine = False
+            # If we are doing coarse search.
+            if not self.fine:
+                # Find the right learning rate (if there is one).
+                lrs = []
+                while len(lrs) < 3:
+                    lr = 10 ** np.uniform(-6, 1)
+                    if lr in lrs: continue
+                    optimizer = optim.SGD(model.parameters(), lr=lr,
+                                          momentum=0.9, nesterov=True)
+                    history = self.trainer(model, optimizer, self.loader_train,
+                                           self.loader_val, epochs=1,
+                                           its=self.coarse_its, eval_its=16)
+                    # If it's working, add it to our active set and break.
+                    if history.working:
+                        self.active.put((-history.val_acc, model, optimizer, history))
+                        break
+            # If we are doing fine search.
+            else:
+                _, model, optimizer, history = self.active.get()
+                history = self.trainer(model, optimizer, self.loader_train,
+                                       self.loader_val, epochs=self.fine_epochs,
+                                       eval_its=16)
+                self.trained.put((-history.val_acc, model, history))
+        else:
+            pass # TODO: other coarse/fine strategies.
+
+    def sample(self):
+        return dict([(n, p.sample()) for (n, s) in self.samplers.items()])
+
+    def update(parameters, hist):
+        [self.samplers[n].update(hist.bad, v) for (n, v) in parameters.items()]
